dee50ef4f66537a891b8188835242a977f164bcb
faker2903 <ace2903722@gmail.com>
2025-11-15 11:58:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ dotenv.config({ path: './.env' });
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "https://sweet-shop-steel.vercel.app/" || "http://localhost:5173",
+    credentials: true
+}));
+
 app.use(express.json());
 
 // Connect to MongoDB
