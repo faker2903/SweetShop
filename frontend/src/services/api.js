0dbd3c8837cea6bce83a434ed0db4fd27bc57b5b
faker2903 <ace2903722@gmail.com>
2025-11-15 12:18:55 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://sweet-shop-y6vp.vercel.app/api';
+const API_BASE_URL = 'https://sweet-shop-y6vp.vercel.app/';
 
 // Create axios instance
 const api = axios.create({
